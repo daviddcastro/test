b500db81a5568e55ba049107afabd0a4b2be0ef9
daviddcastro <daviddcastro03@gmail.com>
2023-01-23 12:57:31 +0100

    o

---
+++ src/html/_data/recomendaciones.js
@@ -0,0 +1,81 @@
+module.exports = [
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Hotel Abetos del Maestre Escuela" ,
+        ciudadProvincia: "Cordoba" ,
+        precio: 146
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Hotel Cibeles Playa" ,
+        ciudadProvincia: "Gandía, Valencia" ,
+        precio: 122
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Axel Hotel Madrid" ,
+        ciudadProvincia: "Madrid" ,
+        precio: 178
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Motel One Barcelona-Ciutadella" ,
+        ciudadProvincia: "Sant Martí, Barcelona" ,
+        precio: 155
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Hilton Garden Inn Sevilla" ,
+        ciudadProvincia: "Sevilla" ,
+        precio: 118
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Holiday Inn Express Málaga Airport, an IHG Hotel" ,
+        ciudadProvincia: "Churriana, Málaga" ,
+        precio: 75
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Sonder Atala Champs-Élysées" ,
+        ciudadProvincia: "Campos Elíseos, París" ,
+        precio: 430
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "La Griffe Roma" ,
+        ciudadProvincia: "Roma" ,
+        precio: 260
+    } ,
+    {
+        imagen: "" ,
+        nombreAlojamiento: "Royal Hotel" ,
+        ciudadProvincia: "Brent, Londres" ,
+        precio: 103
+    } , 
+    {
+        imagen: "" ,
+        ciudad: "" ,
+        banderaPais: ""
+    } ,
+    {
+        imagen: "" ,
+        ciudad: "" ,
+        banderaPais: ""
+    } ,
+    {
+        imagen: "" ,
+        ciudad: "" ,
+        banderaPais: ""
+    } ,
+    {
+        imagen: "" ,
+        ciudad: "" ,
+        banderaPais: ""
+    } ,
+    {
+        imagen: "" ,
+        ciudad: "" ,
+        banderaPais: ""
+    }
+]
