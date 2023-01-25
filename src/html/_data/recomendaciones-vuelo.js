62610f5a253ee499f482cb08234c18fed4c8612d
daviddcastro <daviddcastro03@gmail.com>
2023-01-25 12:44:07 +0100

    añadir macros y datos

---
+++ src/html/_data/recomendaciones-vuelo.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        imagen: "/images/barcelona.png" ,
+        ciudad: "Barcelona" ,
+        bandera: "🇪🇸" ,
+        precio: 59.99
+    } ,
+    {
+        imagen: "/images/madrid.png" ,
+        ciudad: "Madrid" ,
+        bandera: "🇪🇸" ,
+        precio: 79.99
+    } ,
+    {
+        imagen: "/images/londres.png" ,
+        ciudad: "Londres" ,
+        bandera: "🇬🇧" ,
+        precio: 99.99
+    } ,
+    {
+        imagen: "/images/roma.png" ,
+        ciudad: "Roma" ,
+        bandera: "🇮🇹" ,
+        precio: 119.99
+    } ,
+    {
+        imagen: "/images/paris.png" ,
+        ciudad: "Paris" ,
+        bandera: "🇫🇷" ,
+        precio: 149.99
+    }
+]
--- src/html/_data/vuelos.js
+++ src/html/_data/vuelos.js
@@ -1,37 +1,24 @@
 module.exports = [
     {
-        nombre: "",
-        ref: "S23PT0120-245",
-        color: "arena",
-        precio: 35.95,
-        tallas: ["XS", "S", "M", "L", "XL", "2XL", "3XL"],
-        alturaModelo: 1.82,
-        tallaModelo: "S",
-        decripcion: "Jersey de punto confeccionado en tejido 100% algodón. Corte recto, cuello alzado, abertura con cierre de media cremallera personalizada. Coderas grabadas con nuestro caballo y bordado en el pecho.",
-        image: "https://lagarrocha.imgix.net/articulos2/2515/2/media-cremallera-con-coderas--arena.jpg?w=600&auto=format&q=80",
-        galeriaImagenes:[
-            "https://lagarrocha.imgix.net/articulos2/2515/2/media-cremallera-con-coderas--arena.jpg?w=600&auto=format&q=80",
-            "https://lagarrocha.imgix.net/articulos2/2515/2/media-cremallera-con-coderas--arena.jpg?w=600&auto=format&q=80",
-            "https://lagarrocha.imgix.net/articulos2/2515/2/media-cremallera-con-coderas--arena.jpg?w=600&auto=format&q=80",
-            "https://lagarrocha.imgix.net/articulos2/2515/2/media-cremallera-con-coderas--arena.jpg?w=600&auto=format&q=80",
-            "https://lagarrocha.imgix.net/articulos2/2515/2/media-cremallera-con-coderas--arena.jpg?w=600&auto=format&q=80"
-        ],
-        productosRelacionados: [
-            {
-                name: "",
-                precio: 39.95,
-                tallas: [],
-                imagen: ""
-            }
-        ],
-        otrasOpcionesColor: [
-            {
-                imagen: "",
-                color: "",
-                precio: 39.95
-            }
-        ]
-
+        imagenCompañiaIda: "" ,
+        horaSalidaIda: "10:15" ,
+        origenIda: "" ,
+        fechaSalidaIda: "" ,
+        tiempoIda: "" ,
+        directoIda: "" ,
+        horaLlegadaIda: "" ,
+        destinoIda: "" ,
+        fechaLlegadaIda: "" ,
+        imagenCompañiaVuelta: "" ,
+        horaSalidaVuelta: "" ,
+        origenVuelta: "" ,
+        fechaSalidaVuelta: "" ,
+        tiempoVuelta: "" ,
+        directoVuelta: "" ,
+        horaLlegadaVuelta: "" ,
+        destinoVuelta: "" ,
+        fechaLlegadaVuelta: "" ,
+        precio: "" ,
+        producto: ""
     }
 ]
-
